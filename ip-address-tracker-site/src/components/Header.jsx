9ca3e32bf409881d648899d1c8a3e83a25b6ec85
bwalshin <brandon.walshin@gmail.com>
2022-09-25 18:03:43 -0700

    Update states call abbr. Update plate info

--- ip-address-tracker-site/src/components/Header.jsx
+++ ip-address-tracker-site/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
-import '../App.css';
 import { useFormik, FormikProvider, Field } from 'formik';
+import { useQuery } from '@tanstack/react-query';
+
+import '../App.css';
 import { AppContext } from '../App';
 
 export default function Header() {
@@ -11,6 +13,31 @@ export default function Header() {
       setApp(values);
     },
   });
+
+  const { data } = useQuery(
+    ['states'],
+    () =>
+      fetch(
+        'https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json'
+      )
+        .then((res) => res.json())
+        .then((states) =>
+          Object.keys(states).reduce(
+            (sts, key) => ({ ...sts, [states[key]]: key }),
+            {}
+          )
+        ),
+    {
+      cacheTime: Infinity,
+      refecthOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    }
+  );
+
+  if (!data) return null;
+
+  console.log(data);
 
   return (
     <FormikProvider value={formik}>
@@ -30,16 +57,27 @@ export default function Header() {
           <div className='plate'>
             <div className='container'>
               <div className='row'>
-                <h4 className='col'>IP Address</h4>
-                <h4 className='col'>Location</h4>
-                <h4 className='col'>Time Zone</h4>
-                <h4 className='col'>ISP</h4>
-              </div>
-              <div className='row'>
-                <h1 className='col'>{app?.ipData?.id}</h1>
-                <h1 className='col'>city, region, postcalCode</h1>
-                <h1 className='col'>location</h1>
-                <h1 className='col'>isp</h1>
+                <div className='col '>
+                  <h4>IP Address</h4>
+                  <h1>{app?.ipData?.ip}</h1>
+                </div>
+                <div className='col'>
+                  <h4>Location</h4>
+                  <h1>
+                    {app?.ipData?.location?.city}, 
+                    {data[app?.ipData?.location?.region] || app?.ipData?.location?.region}
+                    <br />
+                    {app?.ipData?.location?.postalCode}
+                  </h1>
+                </div>
+                <div className='col'>
+                  <h4>Time Zone</h4>
+                  <h1>UTC {app?.ipData?.location?.timezone}</h1>
+                </div>
+                <div className='col'>
+                  <h4>ISP</h4>
+                  <h1>{app?.ipData?.isp}</h1>
+                </div>
               </div>
             </div>
           </div>
--- ip-address-tracker-site/src/components/IPcall.jsx
+++ ip-address-tracker-site/src/components/IPcall.jsx
@@ -5,16 +5,16 @@ import { AppContext } from '../App';
 
 export default function IPcall() {
   const { app, setApp } = useContext(AppContext);
+  const ipAddress = app?.input || '8.8.8.8';
+  console.log('ipAddress', ipAddress);
   const { status, data, error, isFetching } = useQuery(
-    [app.input], () =>
+    [ipAddress],
+    () =>
       fetch(
-        `https://geo.ipify.org/api/v2/country,city?apiKey=at_aJaHvmwrQfEpHVc04N77e2UIbFBNX&ipAddress=${app.input}`
-        // "https://geo.ipify.org/api/v2/country,city?apiKey=at_aJaHvmwrQfEpHVc04N77e2UIbFBNX&ipAddress=8.8.8.8"
-        // `https://geo.ipify.org/api/v2/country,city?`
+        `https://geo.ipify.org/api/v2/country,city?apiKey=at_aJaHvmwrQfEpHVc04N77e2UIbFBNX&ipAddress=${ipAddress}`
       )
         .then((res) => res.json())
-        // .then((ipData... line is throwing "undefined" error
-        .then((ipData) => setApp((curr) => ({ ...curr, ipData }))),
+        .then((ipData) => setApp((curr) => ({ ...curr, ipData })) || ipData),
     {
       cacheTime: Infinity,
       refecthOnMount: false,
@@ -24,7 +24,7 @@ export default function IPcall() {
   );
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div className='plate'>Error: {error.message}</div>;
   }
 
   if (status === 'loading') {
